3ba019f42dcff58ac84170e4863562781bd5d5b1
Nupoor Shetye <shetyenupoor10@gmail.com>
2023-12-20 11:37:34 +0530

    Implementing text summary endpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require("./config/connectDB");
 const userRoutes = require("./routes/userRoutes");
+const summaryRoutes = require('./routes/summaryRoutes');
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -17,7 +18,8 @@ app.get("/", async(req, res) => {
     res.send('Welcome to the lexilearn server');
 });
 
-app.use("/api/user", userRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/summary", summaryRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started at Port : ${PORT}`);
