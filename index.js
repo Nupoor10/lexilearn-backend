b5e665509592da3ed28577ff5d5cdca43981bc43
Nupoor Shetye <shetyenupoor10@gmail.com>
2023-12-20 14:27:15 +0530

    Implementing speech to text endpoint

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/connectDB");
 const userRoutes = require("./routes/userRoutes");
 const summaryRoutes = require('./routes/summaryRoutes');
 const textToSpeechRoutes = require('./routes/textToSpeechRoutes');
+const speechToTextRoutes = require('./routes/speechToTextRoutes');
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -22,6 +23,7 @@ app.get("/", async(req, res) => {
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/summary", summaryRoutes);
 app.use("/api/v1/texttospeech", textToSpeechRoutes);
+app.use("/api/v1/speechtotext", speechToTextRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started at Port : ${PORT}`);
